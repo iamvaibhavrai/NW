54d0161454b62c37d7971305e65dd828377f387f
iamvaibhavrai <vaibhav.rai@cosm.com>
2023-10-14 22:25:38 +0530

    updated home button linking

--- src/pages/HomeButton.tsx
+++ src/pages/HomeButton.tsx
@@ -7,7 +7,7 @@ const HomeButton = () => {
     <button
       type="button"
       className="shadow-xl border border-gray-300 px-7 py-7 rounded-lg z-50 font-bold text-4xl absolute right-10 top-10"
-      onClick={() => navigate("/splash_screen")}
+      onClick={() => navigate("/")}
     >
       <AiFillHome />
     </button>
